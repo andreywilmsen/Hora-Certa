c2b84114139fc4ccaeb8ccfc87cad94662e3e52a
andreywilmsen <andreywilmsendepaula@gmail.com>
2026-01-03 20:23:38 -0300

    feat: cria useCase createUser

---
+++ modules/user/application/usecases/CreateUser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\modules\user\application\usecases;
+
+use app\modules\user\contracts\repositories\UserRepository;
+use app\modules\user\domain\entities\User;
+use DomainException;
+
+class CreateUser
+{
+
+  private UserRepository $repository;
+
+  public function __construct(UserRepository $repository)
+  {
+    $this->repository = $repository;
+  }
+
+  /** 
+   * @param string $username
+   * @param string $firstName
+   * @param string $lastName
+   * @param string $email
+   * @param string $plainPassword
+   * @return User
+   * @throws DomainException se o e-mail já estiver cadastrado 
+   */
+
+  public function execute(string $userName, string $firstName, string $lastName, string $email, string $plainPassword): User
+  {
+
+    if ($this->repository->findByEmail($email)) {
+      throw new DomainException("E-mail already in use.");
+    }
+
+    $passwordHash = password_hash($plainPassword, PASSWORD_BCRYPT);
+
+    $user = new User(
+      $userName,
+      $firstName,
+      $lastName,
+      $email,
+      $passwordHash
+    );
+
+    $this->repository->save($user);
+
+    return $user;
+  }
+}
